f30fbc7c8a0f9b1de304f0cba328a4f2cf710482
pratha-lukhi <lukhipratha@gmail.com>
2026-09-09 16:47:35 +0530

    Add files via upload

---
+++ python/01_intro/01_hello.py
@@ -0,0 +1,3 @@
+print("hello")
+print("good evening")
+print("whats up?")
---
+++ python/02_print_variable/02_variable.py
@@ -0,0 +1,23 @@
+name = input("enter your name:- ")
+
+print(name)
+
+
+age = int(input("enter your age:- "))
+
+
+print("your age is ",age)
+
+
+# variables rules
+
+
+# 01_name=""
+
+# student name=""
+
+# $variable="" 
+
+# personName=""
+
+# person_name=""
