69745f0016f1c0fd4f4e1afec34c1f33b6d5aeac
pratha-lukhi <lukhipratha@gmail.com>
2026-09-13 23:13:18 +0530

    Add files via upload

---
+++ 04_variable2.py
@@ -0,0 +1,16 @@
+print("Hello")
+
+
+name =input("enter your name:- pratha")
+
+print(name)
+
+
+age =input("enter your age:- 18")
+
+print(age)
+
+
+hobby =("enter your hobby:- play rass")
+
+print(hobby)
