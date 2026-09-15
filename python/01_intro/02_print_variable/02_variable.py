772c28cc50ee1d7433b27af027787bd449464a79
pratha-lukhi <lukhipratha@gmail.com>
2026-09-16 01:37:54 +0530

    Add files via upload

---
+++ python/01_intro/02_print_variable/02_variable.py
@@ -0,0 +1,23 @@
+name = input("enter your name:- ")
+
+print(name)
+
+
+age = input("enter your age:- ")
+
+
+print("your age is ",age)
+
+
+# variables rules
+
+
+# 01_name=""
+
+# student name=""
+
+# $variable="" 
+
+# personName=""
+
+# person_name=""
---
+++ python/01_intro/02_print_variable/03_datatypes.py
@@ -0,0 +1,71 @@
+# 1. String
+
+movieName = "om shanti om"
+
+main_character = "Shah Rukh Khan"
+
+print("movieName data type", type(movieName))
+
+print("main character", main_character)
+
+print("main character data type", type(main_character))
+
+
+# 2. Integer
+
+whole_number = 7
+
+print("number", whole_number)
+
+print("number type", type(whole_number))
+
+
+# 3. Float
+
+decimal_number = 8.5
+print("number", decimal_number)
+
+print("number type", type(decimal_number))
+
+
+# 4. boolean
+
+isLoggedIn = True
+
+print("is user logged in", isLoggedIn)
+
+print("is user logged datatype", type(isLoggedIn))
+
+
+verified = False
+
+print("is user account is verified", verified)
+
+print("verified data type", type(verified))
+
+# 5. list => mutable => can be changed
+
+fruits = ["apple", "mango", "cherry"]
+
+print("fruits items",fruits)
+
+print("fruits datatypes",type(fruits))
+
+fruits[0]="pineapple"
+
+print("fruits items after",fruits)
+
+print("fruits data types",type(fruits))
+
+
+# 6.  tuple  =>immutable => can't be changed
+
+vegetables = {"potato","cucumber","garlic","tomato"}
+
+print("vegetable before",vegetables)
+
+# vegetables[0]="onion"
+
+print("vegetable after",vegetables)
+
+print("vegetable dataTypes",type(vegetables))
